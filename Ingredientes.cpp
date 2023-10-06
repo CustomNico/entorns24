8cdbe80cf3192cf7b4e519b6e803fc6ee69d1eba
Gerard <gllompartsallegi@gmail.com>
2023-10-06 09:55:39 +0200

    crema catalana

---
+++ Ingredientes.cpp
@@ -0,0 +1,32 @@
+Ingredientes
+Para 6 personas
+Leche entera
+1 l
+Yemas de huevo
+8
+Azúcar
+100 g
+Maicena
+20 g
+Corteza de limón al gusto
+Corteza de naranja al gusto
+Azúcar para caramelizar al gusto
+Canela en rama
+1
+Cómo hacer crema catalana
+Dificultad: Fácil
+Tiempo total
+20 m
+Elaboración
+10 m
+Cocción
+10 m
+Comenzaremos cascando los huevos y separando la yema de las claras. Estas no las tiréis, podéis aprovecharlas para otras preparaciones.
+
+En una olla ponemos la leche a calentar con la corteza de limón, de naranja y la rama de canela. Por otra parte en un bol mezclamos removiendo bien, el azúcar y la Maicena, añadimos las yemas batidas y unimos todo con unas varillas.
+
+Cuando la leche rompa a hervir vertemos la mezcla de yemas, azúcar y maicena que la pasaremos por un chino o colador grueso. Removemos a fuego medio y sin parar de remover con una varilla para que no se nos pegue hasta que la mezcla vuelva a hervir de nuevo. Volcamos en unas cazuelitas individuales.
+
+Dejamos enfriar sin tapar y espolvoreamos con el resto del azúcar que habremos repartido entre nuestras cremas. Requemamos con un soplete de cocina hasta que el azúcar se caramelice. Servir fría.
+
+crema catalana
